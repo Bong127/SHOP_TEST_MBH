5453c0916ffe24a2f3b5e282d434b0451f2b884a
Bong127 <ansqhdgus200@naver.com>
2024-10-30 22:26:32 +0900

    userDao 구현

--- Shop_Test_MBH/src/main/java/shop/dao/UserRepository.java
+++ Shop_Test_MBH/src/main/java/shop/dao/UserRepository.java
@@ -1,23 +1,93 @@
 package shop.dao;
 
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.UUID;
-
 import shop.dto.PersistentLogin;
-import shop.dto.Product;
 import shop.dto.User;
 
 public class UserRepository extends JDBConnection {
+	
+	public User map(ResultSet rs) throws Exception {
+		User user = new User();
+    	user.setId( rs.getString("id")); 
+    	user.setPassword( rs.getString("password")); 
+    	user.setName( rs.getString("name")); 
+    	user.setGender( rs.getString("gender")); 
+    	user.setBirth( rs.getString("birth")); 
+    	user.setMail( rs.getString("mail")); 
+    	user.setPhone( rs.getString("phone")); 
+    	user.setAddress( rs.getString("address")); 
+    	user.setRegistDay( rs.getString("regist_day")); 
+		return user;
+	}
+	
 	
 	/**
 	 * 회원 등록
 	 * @param user
 	 * @return
 	 */
-	public int insert(User user) {
-		String sql = "insert into user WHERE id = ?";
-		return 0;
+	public int insert(User user) throws Exception{
+		 int result = 0;
+	        StringBuilder sql = new StringBuilder("INSERT INTO user (");
+	        StringBuilder placeholders = new StringBuilder(" VALUES (");
+	        
+	        Field[] fields = user.getClass().getDeclaredFields();
+	        boolean first = true;
+
+	        for (Field field : fields) {
+	            field.setAccessible(true);
+	            Object value = field.get(user);
+
+	            if (value != null) {
+	                if (!first) {
+	                    sql.append(", ");
+	                    placeholders.append(", ");
+	                }
+					String fieldName = field.getName();
+	                sql.append(fieldName);
+	                placeholders.append("?");
+	                first = false;
+	            }
+	        }
+
+	        sql.append(") ");
+	        placeholders.append(")");
+	        sql.append(placeholders.toString());
+	        
+	        try {
+	            psmt = con.prepareStatement(sql.toString());
+	            int index = 1;
+
+	            for (Field field : fields) {
+	                field.setAccessible(true);
+	                Object value = field.get(user);
+
+	                if (value != null) {
+	                    if (value instanceof String) {
+	                        psmt.setString(index++, (String) value);
+	                    } else if (value instanceof Boolean) {
+	                        psmt.setBoolean(index++, (Boolean) value);
+	                    } else if (value instanceof Long) {
+	                        psmt.setLong(index++, (Long) value);
+	                    } else if (value instanceof Integer) {
+	                        psmt.setInt(index++, (Integer) value);
+	                    } else if (value instanceof Date) {
+	                        psmt.setDate(index++, new java.sql.Date(((Date) value).getTime()));
+	                    } else {
+	                        psmt.setObject(index++, value);
+	                    }
+	                }
+	            }
+	            result = psmt.executeUpdate();
+	        } catch (Exception e) {
+	            System.err.println("user - insert 도중 에러");
+	            e.printStackTrace();
+	        }
+	        return result;
 	}
 	
 	
@@ -26,10 +96,25 @@ public class UserRepository extends JDBConnection {
 	 * @param id
 	 * @param pw
 	 * @return
+	 * @throws Exception 
 	 */
-	public User login(String id, String pw) {
-		return null;
-		
+	public User login(String id, String pw) throws Exception {
+		String sql = "SELECT * FROM user WHERE id = ? and password = ?";
+	    User user = null;
+	    try {
+	        psmt = con.prepareStatement(sql);
+	        psmt.setString(1, id);
+	        psmt.setString(2, pw);
+
+	        rs = psmt.executeQuery();
+	        if(rs.next())
+	        	user = map(rs);
+	        rs.close();
+	    } catch (SQLException e) {
+	        System.err.println("로그인 실패!");
+	        e.printStackTrace();
+	    }
+	    return user;
 	}
 	
 	
@@ -40,8 +125,9 @@ public class UserRepository extends JDBConnection {
 	 * @param id
 	 * @param pw
 	 * @return
+	 * @throws Exception 
 	 */
-	public User getUserById(String id) {
+	public User getUserById(String id) throws Exception {
 	    String sql = "SELECT * FROM user WHERE id = ?";
 	    
 	    User user = null;
@@ -50,21 +136,11 @@ public class UserRepository extends JDBConnection {
 	        psmt.setString(1, id);
 
 	        rs = psmt.executeQuery();
-	        if (rs.next()) {
-	        	user = new User();
-	        	user.setId( rs.getString("id")); 
-	        	user.setPassword( rs.getString("password")); 
-	        	user.setName( rs.getString("name")); 
-	        	user.setGender( rs.getString("gender")); 
-	        	user.setBirth( rs.getString("birth")); 
-	        	user.setMail( rs.getString("mail")); 
-	        	user.setPhone( rs.getString("phone")); 
-	        	user.setAddress( rs.getString("address")); 
-	        	user.setRegistDay( rs.getString("regist_day")); 
-	        }
+	        if(rs.next())
+	        	user = map(rs);
 	        rs.close();
 	    } catch (SQLException e) {
-	        System.err.println("자동 로그인 정보 조회 중, 에러 발생!");
+	        System.err.println("로그인 정보 조회 중, 에러 발생!");
 	        e.printStackTrace();
 	    }
 	    return user;
@@ -76,9 +152,87 @@ public class UserRepository extends JDBConnection {
 	 * @param user
 	 * @return
 	 */
-	public int update(User user) {
-		return 0;
-		
+	public int update(User user) throws Exception {
+        int result = 0;
+        StringBuilder sql = new StringBuilder("UPDATE user SET ");
+        StringBuilder whereClause = new StringBuilder(" WHERE id = ?");
+
+        Field[] fields = user.getClass().getDeclaredFields();
+        boolean first = true;
+        Object pkValue = null;
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(user);
+            if (field.getName().equals("id")) {
+                pkValue = value;
+                continue;
+            }
+            if (value != null) {
+                if (!first) {
+                    sql.append(", ");
+                }
+                String fieldName = field.getName();
+                sql.append(fieldName).append(" = ?");
+                first = false;
+            }
+        }
+        sql.append(whereClause);
+        try {
+            psmt = con.prepareStatement(sql.toString());
+            int index = 1;
+
+            for (Field field : fields) {
+                field.setAccessible(true);
+                Object value = field.get(user);
+                if (field.getName().equals("id")) {
+                    continue;
+                }
+                if (value != null) {
+                    if (value instanceof String) {
+                        psmt.setString(index++, (String) value);
+                    } else if (value instanceof Boolean) {
+                        psmt.setBoolean(index++, (Boolean) value);
+                    } else if (value instanceof Long) {
+                        psmt.setLong(index++, (Long) value);
+                    } else if (value instanceof Integer) {
+                        psmt.setInt(index++, (Integer) value);
+                    } else if (value instanceof Double) {
+                        psmt.setDouble(index++, (Double) value);
+                    } else if (value instanceof Float) {
+                        psmt.setFloat(index++, (Float) value);
+                    } else if (value instanceof java.util.Date) {
+                        psmt.setDate(index++, new java.sql.Date(((java.util.Date) value).getTime()));
+                    } else {
+                        psmt.setObject(index++, value);
+                    }
+                }
+            }
+
+            // WHERE pk = ? <-- 조건 값 지정
+            if (pkValue instanceof String) {
+                psmt.setString(index, (String) pkValue);
+            } else if (pkValue instanceof Boolean) {
+                psmt.setBoolean(index, (Boolean) pkValue);
+            } else if (pkValue instanceof Long) {
+                psmt.setLong(index, (Long) pkValue);
+            } else if (pkValue instanceof Integer) {
+                psmt.setInt(index, (Integer) pkValue);
+            } else if (pkValue instanceof Double) {
+                psmt.setDouble(index, (Double) pkValue);
+            } else if (pkValue instanceof Float) {
+                psmt.setFloat(index, (Float) pkValue);
+            } else if (pkValue instanceof java.util.Date) {
+                psmt.setDate(index, new java.sql.Date(((java.util.Date) pkValue).getTime()));
+            } else {
+                psmt.setObject(index, pkValue);
+            }
+            result = psmt.executeUpdate();
+        } catch (Exception e) {
+            System.err.println("user - update 도중 에러");
+            e.printStackTrace();
+        }
+        return result;
 	}
 
 
@@ -88,8 +242,16 @@ public class UserRepository extends JDBConnection {
 	 * @return
 	 */
 	public int delete(String id) {
-		return 0;
-		
+		int result = 0;
+		String sql = "delete from user where id = ?";
+		try {
+			psmt = con.prepareStatement(sql);
+			result = psmt.executeUpdate();
+		} catch (SQLException e) {
+			System.err.println("user - delete 도중 에러");
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 	/**
